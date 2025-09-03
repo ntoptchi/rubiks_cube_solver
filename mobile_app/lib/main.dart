1e399875239553615096874ee4b401a4f3c57947
Nick <nick.fl0005@gmail.com>
2025-09-02 21:46:45 -0400

    Update main.dart

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -3,28 +3,27 @@ import 'pages/input_page.dart';
 import 'pages/camera_page.dart';
 import 'pages/cube_viewer.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Rubik Solver',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const InputPage(),
+      initialRoute: '/',
       routes: {
+        '/': (_) => const InputPage(),
         '/camera': (_) => const CameraPage(),
         '/viewer': (ctx) {
-          final textureMap = Map<String, String>.from(
-            ModalRoute.of(ctx)!.settings.arguments as Map,
-          );
-          return CubeViewer(textureUrls: textureMap);
+          final urls = ModalRoute.of(ctx)!.settings.arguments as Map<String, String>;
+          return CubeViewer(textureUrls: urls);
         },
       },
+      // Optional safety net so unknown routes don’t crash:
+      onUnknownRoute: (_) => MaterialPageRoute(builder: (_) => const InputPage()),
     );
   }
 }
